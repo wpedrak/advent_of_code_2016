e1302cb07907d250b6f591e9592b4e1ac9734052
Wojciech Pedrak <woj123@gmail.com>
2020-04-20 18:46:46 +0200

    15.1 done

---
+++ 15/solution.py
@@ -0,0 +1,23 @@
+discs = [
+    [13, 10],
+    [17, 15],
+    [19, 17],
+    [7, 1],
+    [5, 0],
+    [3, 1]
+]
+
+for time, disc in enumerate(discs):
+    disc[1] += time + 1
+
+propositions = [0] * int(10**6)
+
+for disc in discs:
+    period, shift = disc
+    start = period - shift
+
+    for idx in range(start, len(propositions), period):
+        propositions[idx] += 1
+
+result = propositions.index(len(discs))
+print(result)
